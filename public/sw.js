a8cc1b4a4759352f81abfabd1f27830328851e16
Alpha <alpha@alphaos.local>
2026-07-16 23:23:55 +0200

    chore: commit build artifacts

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // fitness-dev Service Worker
 // v1: static cache + stale-while-revalidate reads + Background Sync für offline POSTs
 
-const CACHE = 'vitalos-v148'
+const CACHE = 'vitalos-v149'
 
 const STATIC = [
   '/',
