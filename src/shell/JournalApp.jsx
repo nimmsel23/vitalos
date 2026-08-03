7eda5e28b612efdd58a5a0ff9d5083d25e05306b
Alpha <alpha@alphaos.local>
2026-08-03 12:14:54 +0200

    fix(layout): embed journal wrappers cleanly in vitalos

--- src/shell/JournalApp.jsx
+++ src/shell/JournalApp.jsx
@@ -3,7 +3,7 @@ import Journal from '@view/journal'
 export default function JournalApp({ onOpenSession }) {
   return (
     <div className="h-full overflow-y-auto p-4 sm:p-8 lg:p-12 max-w-[1600px] mx-auto">
-      <Journal onOpenSession={onOpenSession} />
+      <Journal embedded onOpenSession={onOpenSession} />
     </div>
   )
 }
